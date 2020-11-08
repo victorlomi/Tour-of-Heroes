270a5ac750562d950fd49eb173cfaf5c7793d91b
plutos28 <gamevictor28@gmail.com>
2020-11-08 21:06:38 +0300

    display list of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero} from './hero'
+
+export const HEROES: Hero[] = [
+  {id: 11, name: "Dr Nice"},
+  {id: 12, name: "Narco"},
+  {id: 13, name: "Bombasto"},
+  {id: 14, name: "Celeritas"},
+  {id: 15, name: "Magneta"},
+  {id: 16, name: "Rubberman"},
+  {id: 17, name: "Luffy"},
+  {id: 18, name: "Zainer"},
+  {id: 19, name: "IQ"},
+  {id: 20, name: "Tornado"},
+]
